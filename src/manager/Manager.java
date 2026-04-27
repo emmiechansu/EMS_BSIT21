cf23225144ee27b9278b5cdc99c6f3b346c6c79b
MJ Pepito <mj.pepito.01@gmail.com>
2026-04-27 14:55:42 +0800

    Initial commit

---
+++ src/manager/Manager.java
@@ -0,0 +1,16 @@
+package manager;
+
+
+import javax.swing.*;
+
+public class Manager {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       Manager_DB m = new Manager_DB();
+       m.setVisible(true);
+    }
+    
+}
---
+++ src/manager/Manager_DB.java
@@ -0,0 +1,91 @@
+package manager;
+
+import javax.swing.*;
+import java.awt.event.*;
+
+public class Manager_DB extends JFrame {
+
+    JList<String> requestList;
+
+    String[] requests = {
+            "1 - Charles Ynares | Leave | Pending",
+            "2 - Matthew Fernandez | Expense | Pending"
+    };
+
+    public Manager_DB(){
+
+        setTitle("Manager Dashboard");
+        setSize(600,400);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(null);
+
+        JButton dashboardBtn = new JButton("Dashboard");
+        dashboardBtn.setBounds(20,30,120,30);
+
+        JButton employeeBtn = new JButton("Employee List");
+        employeeBtn.setBounds(20,70,120,30);
+
+        JButton requestBtn = new JButton("Requests");
+        requestBtn.setBounds(20,110,120,30);
+
+        JButton logoutBtn = new JButton("Logout");
+        logoutBtn.setBounds(20,150,120,30);
+
+        add(dashboardBtn);
+        add(employeeBtn);
+        add(requestBtn);
+        add(logoutBtn);
+
+        JLabel label = new JLabel("Employee Requests");
+        label.setBounds(200,20,200,30);
+        add(label);
+
+        JLabel identifierlbl = new JLabel("Employee ID | Name | Type | Status");
+        identifierlbl.setBounds(200,50,350,30);
+        add(identifierlbl);
+
+        requestList = new JList<>(requests);
+
+        JScrollPane scroll = new JScrollPane(requestList);
+        scroll.setBounds(200,80,300,150); 
+        add(scroll);
+
+        JButton approveBtn = new JButton("Approve");
+        approveBtn.setBounds(200,250,100,30);
+
+        approveBtn.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+
+                int index = requestList.getSelectedIndex();
+
+                if(index != -1){
+                    requests[index] = requests[index].replace("Pending","Approved");
+                    requestList.setListData(requests);
+                } else {
+                    JOptionPane.showMessageDialog(null,"Select an employee first.");
+                }
+            }
+        });
+
+        add(approveBtn);
+
+        JButton rejectBtn = new JButton("Reject");
+        rejectBtn.setBounds(320,250,100,30);
+
+        rejectBtn.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+
+                int index = requestList.getSelectedIndex();
+
+                if(index != -1){
+                    requests[index] = requests[index].replace("Pending","Rejected");
+                    requestList.setListData(requests);
+                } else {
+                    JOptionPane.showMessageDialog(null,"Select an employee first.");
+                }
+            }
+        });
+
+        add(rejectBtn);
+    }
+}
