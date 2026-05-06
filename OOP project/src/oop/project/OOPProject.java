429f4d5a978a5ee3b250821396056af256d8fef2
Lourd <lourdlealcruz@gmail.com>
2026-05-06 19:12:10 +0800

    new work

---
+++ OOP project/src/oop/project/OOPProject.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package oop.project;
+
+/**
+ *
+ * @author Lourd Leal Cruz
+ */
+public class OOPProject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       HRDASHBOARD CRUZ = new HRDASHBOARD(); // TODO code application logic here
+    }
+    
+}
