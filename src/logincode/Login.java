ab806a5a5bbdd2f90e28b0e340a556515cad3a11
Brix <sochacojohnbrix13@gmail.com>
2026-05-13 13:57:10 +0800

    Initial Commit

--- src/logincode/Login.java
+++ src/logincode/Login.java
@@ -2,90 +2,82 @@ package logincode;
 import java.awt.event.*;
 import javax.swing.*;
 
+public class Login extends JFrame implements ActionListener {
 
-public class Login extends JFrame implements ActionListener{
-    private JLabel lblLogin,lblUser,lblPass,lblRole;
+    private JLabel lblLogin, lblUser, lblPass, lblRole;
     private JPasswordField psPass;
     private JTextField txtUser;
     private JButton btnLog;
     private JComboBox<String> roleBox;
-    
+
     public Login(){
         setTitle("Login Frame");
         setSize(600, 400);
         setLayout(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        
+        setLocationRelativeTo(null);
+
         lblLogin = new JLabel("LOGIN FRAME", SwingConstants.CENTER);
-        lblLogin.setBounds(150, 40, 250, 10);
+        lblLogin.setBounds(150, 40, 250, 20);
         add(lblLogin);
-        
-        txtUser = new JTextField();
-        txtUser.setBounds(220, 120, 150, 30);
-        add(txtUser);
-        
+
+        lblRole = new JLabel("Role:");
+        lblRole.setBounds(150, 80, 100, 25);
+        add(lblRole);
+
+        String[] roles = {"HR", "Manager", "Employee"};
+        roleBox = new JComboBox<>(roles);
+        roleBox.setBounds(220, 80, 150, 25);
+        add(roleBox);
+
         lblUser = new JLabel("Username:");
         lblUser.setBounds(150, 120, 100, 30);
         add(lblUser);
-        
-        psPass = new JPasswordField();
-        psPass.setBounds(220, 145, 150, 30);
-        add(psPass);
-        
-        lblPass = new JLabel("Password:");
-        lblPass.setBounds(150, 145, 100, 30);
-        add(lblPass);
-        
-        lblRole = new JLabel("Role:", SwingConstants.CENTER);
-        lblRole.setBounds(150, 60, 250, 20);
-        add(lblRole);
-        
-        String[] roles = {"HR", "Manager", "Employee"};
-        roleBox = new JComboBox<>(roles);
-        roleBox.setBounds(150, 80, 250, 25);
-        add(roleBox);
-        
-        btnLog = new JButton("Submit");
-        btnLog.setBounds(220, 185, 100, 30);
-        add(btnLog);
-        
-        btnLog.addActionListener(this);
-                
-        
 
+        txtUser = new JTextField();
+        txtUser.setBounds(220, 120, 150, 30);
+        add(txtUser);
+
+        lblPass = new JLabel("Password:");
+        lblPass.setBounds(150, 165, 100, 30);
+        add(lblPass);
+
+        psPass = new JPasswordField();
+        psPass.setBounds(220, 165, 150, 30);
+        add(psPass);
+
+        btnLog = new JButton("Submit");
+        btnLog.setBounds(220, 215, 100, 30);
+        add(btnLog);
+
+        btnLog.addActionListener(this);
     }
 
     @Override
-public void actionPerformed(ActionEvent e){
-    if (e.getSource() == btnLog) {
-
-        String username = txtUser.getText();
-        String password = new String(psPass.getPassword());
-        String role = (String) roleBox.getSelectedItem();
-
-        // HR Login
-        if (username.equals("HR") && password.equals("12345") && role.equals("HR")) {
-            new MainDashboard(role).setVisible(true);
-            dispose();
-        }
-
-        // Manager Login
-        else if (username.equals("Manager") && password.equals("12345") && role.equals("Manager")) {
-            new MainDashboard(role).setVisible(true);
-            dispose();
-        }
-
-        // Employee Login
-        else if (username.equals("Employee") && password.equals("12345") && role.equals("Employee")) {
-            new EmployeeDashboard (role).setVisible(true);
-            dispose();
-        }
-
-        // Invalid login
-        else {
-            JOptionPane.showMessageDialog(this, "Invalid credentials!");
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == btnLog) {
+            String username = txtUser.getText();
+            String password = new String(psPass.getPassword());
+            String role = (String) roleBox.getSelectedItem();
+            //HR Login 
+            if (username.equals("HR") && password.equals("12345") && role.equals("HR")) {
+                new MainDashboard(role).setVisible(true);
+                dispose();
+            }
+            //Manager Login 
+            else if (username.equals("Manager") && password.equals("12345") && role.equals("Manager")) {
+                new MainDashboard(role).setVisible(true);
+                dispose();
+            }
+            //Employee Login
+            else if (username.equals("Employee") && password.equals("12345") && role.equals("Employee")) {
+                new EmployeeDashboard(username, role).setVisible(true);
+                dispose();
+            }
+            //Invalid Login
+            else {
+                JOptionPane.showMessageDialog(this, "Invalid credentials!");
+            }
         }
     }
-
-   }
 }
