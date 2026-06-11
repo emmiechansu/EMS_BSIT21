470127c7602c48305d8311d979c47a5144fc796d
MJ Pepito <mj.pepito.01@gmail.com>
2026-06-11 08:03:24 +0800

    Direct root project structure

---
+++ src/employee_management_system/Employee_Management_System.java
@@ -0,0 +1,13 @@
+
+package employee_management_system;
+
+
+public class Employee_Management_System {
+
+    public static void main(String[] args) {
+       Login l = new Login();
+       l.setVisible(true);
+        
+    }
+    
+}
